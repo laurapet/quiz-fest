fbd32c28df957ffd3ae29cf82a60fa9d1dab32e8
johan <johanna.bernhard@hs-osnabrueck.de>
2021-09-03 14:17:37 +0200

    selector play

--- src/app/quizzes/tab2.page.ts
+++ src/app/quizzes/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ActionSheetController} from "@ionic/angular";
+import {CategoryService} from "../services/category.service";
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,52 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  currentCategory = 'cat';
+
+  constructor(public actionSheetController: ActionSheetController,public categoryService: CategoryService) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {QuizList} from '../entitys/QuizList';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,12 @@ export class CategoryService {
   constructor() { }
 
   public getAllCategorys(): string[]{
-    return [];
+    return ['Category1', 'Category2', 'Category3', 'Category4'];
   }
 
   public getQuizzesFromCategory(name: string): QuizList[]{
-    return [];
+    return [{title: 'xyz', linkToEdit: '', linkToPlay: ''},
+      {title: 'abc', linkToEdit: '', linkToPlay: ''},
+      {title: 'skdgjosdihgoi sdfgidofihgihdiofhg oiehdoihaiert09eß0risbfgvmnbeaär ituhoiahhtjfgbdfiu hguirheishgibsykjdfbg', linkToEdit: '', linkToPlay: ''}];
   }
 }
