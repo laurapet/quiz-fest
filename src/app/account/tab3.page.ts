9f3691b4523fe490125d34b6f8c8dd3f6b41b016
Laura Peter <laura.peter@hs-osnabrueck.de>
2021-09-08 19:16:39 +0200

    load own quizzes

--- src/app/account/tab3.page.ts
+++ src/app/account/tab3.page.ts
@@ -3,6 +3,8 @@ import { IonButton } from '@ionic/angular';
 import { IonList } from '@ionic/angular';
 import { IonItem } from '@ionic/angular';
 import { IonIcon } from '@ionic/angular';
+import { QuizList } from '../entitys/QuizList';
+import { EditQuizService } from '../services/edit-quiz.service';
 
 @Component({
   selector: 'app-tab3',
@@ -11,6 +13,19 @@ import { IonIcon } from '@ionic/angular';
 })
 export class Tab3Page {
 
-  constructor() {}
+   ownQuizzes: QuizList[];
+
+  constructor(public editQuizService: EditQuizService) {
+    this.loadOwnQuizzes();
+  }
+
+  loadOwnQuizzes(){
+    this.editQuizService.getOwnQuizzes()
+      .subscribe(quizzes =>{
+      this.ownQuizzes = quizzes;
+      }
+    );
+    //this.ownQuizzes = this.editQuizService.getOwnQuizzes();
+  }
 
 }
--- src/app/services/edit-quiz.service.ts
+++ src/app/services/edit-quiz.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { QuizEdit } from '../entitys/QuizEdit';
 import { QuizList } from '../entitys/QuizList';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditQuizService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public createQuiz(quiz: QuizEdit){
 
@@ -17,11 +19,13 @@ export class EditQuizService {
 
   }
 
-  public getOwnQuizzes(): QuizList[]{
+  public getOwnQuizzes(): /**/Observable<QuizList[]>{
+    /*
     let ownQuizzes: QuizList[];
-    ownQuizzes = [{title: 'Naturquiz', linkToEdit: '', linkToPlay: ''},
-    {title: 'abc', linkToEdit: '', linkToPlay: ''},
-    {title: 'Kultur 101', linkToEdit: '', linkToPlay: ''}];
-    return ownQuizzes;
+    ownQuizzes = [{title: 'Naturquiz', linkToEdit: '', linkToQuiz: ''},
+    {title: 'abc', linkToEdit: '', linkToQuiz: ''},
+    {title: 'Kultur 101', linkToEdit: '', linkToQuiz: ''}];
+    return ownQuizzes;*/
+    return (this.http.get<QuizList[]>('/quizzes'));
   }
 }
