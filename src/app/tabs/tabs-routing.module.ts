1122caf603f4dabe94cc74fe3e828758980428c3
johan <johanna.bernhard@hs-osnabrueck.de>
2021-09-22 20:00:50 +0200

    rename tab1 to tab-account

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     children: [
       {
         path: 'settings',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../tab-account/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: 'play',
